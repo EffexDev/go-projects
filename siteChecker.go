ac349f508c56c712dceef9421c9092362632aaf9
EffexDev <effexdev@gmail.com>
2025-08-12 13:24:09 +0800

    Added async pings to each site

--- siteChecker.go
+++ siteChecker.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"os/exec"
+	"sync"
 )
 
 func main() {
 
 	filePath := "site.txt"
 
-	file, err := os.Open(filename)
+	file, err := os.Open(filePath)
 		if err != nil {
 			fmt.Println("Unable to read file")
 		}
@@ -18,10 +20,26 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-	}
+	var pingWG sync.WaitGroup
 
+	for scanner.Scan() {
+		address := scanner.Text()
+		if address =="" {
+			continue
+		}
+		//fmt.Println(line)
+
+		pintWG.Add(1)
+		go func(addr string) {
+			defer pingWG.Done()
+			cmd := exec.Command("cmd", "/C", "ping", addr).CombinedOutput()
+			fmt.Println(string(output))
+		}(address)
+		
+		
+		output, _ := cmd
+		fmt.Println(string(output))
+	}
+	pingWG.Wait()
 }
 
