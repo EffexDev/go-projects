8479e7ac288bfe8bda8c685be61a1729817ce8ad
Effex <jtcartled@gmail.com>
2025-08-20 14:06:21 +0800

    Changed function that pushes jobs to the channel to be asynchronous with the main thread to avoid channel buffer deadlocks

--- siteChecker.go
+++ siteChecker.go
@@ -3,30 +3,29 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"os/exec"
 	"time"
 )
 
 func worker(id int, jobs <-chan string, results chan<- string) {
 	for job := range jobs {
 
-		result, err := exec.Command("cmd", "/C", "ping", job).CombinedOutput()
-		if err != nil {
-			fmt.Println(err)
-		}
+		// result, err := exec.Command("cmd", "/C", "ping", job).CombinedOutput()
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
 
 		//Simulate some time as can't ping using CMD from online Go compiler
 		time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
 
-		results <- fmt.Sprintf("%s", result)
+		results <- fmt.Sprintf("URL %s checked by worker %d", job, id)
 	}
 }
 
 func main() {
-	jobs := make(chan string, 10)
-	results := make(chan string, 10)
+	jobs := make(chan string)
+	results := make(chan string)
 
-	for id := 1; id <= 3; id++ {
+	for id := 1; id <= 5; id++ {
 		go worker(id, jobs, results)
 	}
 
@@ -40,12 +39,41 @@ func main() {
 		"www.telstra.com",
 		"www.optus.com.au",
 		"8.8.8.8",
+		"www.google.com",
+		"www.github.com",
+		"www.facebook.com",
+		"www.x.com",
+		"www.buddytelco.com.au",
+		"www.aussiebroadband.com.au",
+		"www.telstra.com",
+		"www.optus.com.au",
+		"8.8.8.8",
+		"www.google.com",
+		"www.github.com",
+		"www.facebook.com",
+		"www.x.com",
+		"www.buddytelco.com.au",
+		"www.aussiebroadband.com.au",
+		"www.telstra.com",
+		"www.optus.com.au",
+		"8.8.8.8",
+		"www.google.com",
+		"www.github.com",
+		"www.facebook.com",
+		"www.x.com",
+		"www.buddytelco.com.au",
+		"www.aussiebroadband.com.au",
+		"www.telstra.com",
+		"www.optus.com.au",
+		"8.8.8.8",
 	}
 
-	for _, u := range urls {
-		jobs <- u
-	}
-	close(jobs)
+	go func() {
+		for _, u := range urls {
+			jobs <- u
+		}
+		close(jobs)
+	}()
 
 	for i := 0; i < len(urls); i++ {
 		fmt.Println(<-results)
